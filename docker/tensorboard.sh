7b4329335f987c8eb7c10c34cf677ee9e6ed60ef
ozakiryota <kb22056@gmail.com>
2023-02-05 16:49:17 +0900

    Fix docker script

--- docker/tensorboard.sh
+++ docker/tensorboard.sh
@@ -10,15 +10,17 @@ xhost +
 
 image="autoencoder"
 tag="latest"
+home_dir="/home/user"
 
 docker run \
 	-it \
 	--rm \
+	-e local_uid=$(id -u $USER) \
+	-e local_gid=$(id -g $USER) \
 	-e "DISPLAY" \
 	-v "/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--gpus all \
 	--net=host \
 	-p 6006:6006 \
-	-v $(pwd)/../log:/root/$image/log \
+	-v $(pwd)/../log:$home_dir/$image/log \
 	$image:$tag \
-	bash -c "tensorboard --logdir=/root/$image/log/$log_dir"
+	bash -c "tensorboard --logdir=$home_dir/$image/log/$log_dir"
